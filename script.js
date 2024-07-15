0b1c3b304f342eb719dbae4f31476030db179403
Hardy08 <prakashsingh98310@gmail.com>
2024-07-15 17:00:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleDropdown() {
+    var dropdown = document.querySelector('.menu-icon .dropdown-content');
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
+    }
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.menu-icon')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+  }
+  
